01915d8476de936651d9b86eb17c1632f768dd08
Loy Hernandez <loyhernandez59@gmail.com>
2017-10-19 22:09:52 -0500

    collection grid

--- public/components/search/searchCtrl.js
+++ public/components/search/searchCtrl.js
@@ -37,44 +37,7 @@ angular.module('comicApp').controller('searchCtrl', function($scope, searchSrv, 
 
 
    
-    $scope.pageChanged = (newPage) => {
-        console.log(newPage)
-    getResultsPage(newPage)
-
-    }
-    getResultsPage = (pageNumber) => {
-
-        $scope.pagination = {
-            current: 1
-        };
-
-       
-       return $http({
-           method: 'GET',
-           url: 'https://api-comic-vine.herokuapp.com/search/?api_key=dbe5258aa34625d379e5b78516f7364665038eb9&format=json&limit=50&query=' + pageNumber.name + '&page=' + pageNumber.newPageNumber,
-       }).then(function successCallback(data){
-        const issueArr = []
-        // console.log('Y0',data)
-        // console.log(data.data)
-        const book = data.data.results
-        
-        for(var i = 0; i< book.length; i++){
-            //console.log(book[i].resource_type)
-            const issue=book[i];
-            if(issue.resource_type==='issue'){
-                issueArr.push(book[i])
-            }
-        }
-        console.log(issueArr)
-           console.log('a',data.data)
-        $scope.totalbooks = data.data.number_of_total_results
-        $scope.booksPerPage = data.data.limit
-        $scope.comicbook = issueArr
-  
-
-       })
-    }
-
+   
 
 
     $scope.undo = (book) => {
